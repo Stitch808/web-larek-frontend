7df8a884e3401ef548bc87d44dad02ebe8d020ef
Stitch <kondratyuk_serezhenka@inbox.ru>
2024-06-30 01:42:56 +0300

    правки

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,7 @@ export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
 export const settings = {
-
 };
+
+export const API_TOKEN="6135dc50-0dd8-4ab7-b973-a4db17a51ec4"
+export const API_ORIGIN="https://larek-api.nomoreparties.co"
