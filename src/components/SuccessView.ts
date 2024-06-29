a079d78809c146bfa9f75d7cd444ca49b52fdd67
Stitch <kondratyuk_serezhenka@inbox.ru>
2024-06-29 23:19:58 +0300

    правки

--- src/components/SuccessView.ts
+++ src/components/SuccessView.ts
@@ -1,7 +1,5 @@
 import { ensureElement } from "../utils/utils";
 import { Component } from "./base/Component";
-import { IEvents } from "./base/events";
-
 
 interface ISuccess {
     title: string;
